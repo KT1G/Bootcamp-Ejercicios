6eefb5393ef62cc592972d9d3b64500752fc84e4
hacker <braian_bg@hotmail.es>
2022-06-01 18:40:57 +0200

    modificaciones

--- semana-04-javascript/3-Bucles/script.js
+++ semana-04-javascript/3-Bucles/script.js
@@ -1,26 +1,24 @@
 let hora = 7;
+const horasDia = 24
 
 
-for (let i = 0; i < 24; i++) {
+for (let i = 0; i < horasDia; i++) {
 
-    if (i >= 8 && i <= 22) {
+  if (i >= 8 && i <= 22) {
 
-        hora++
-        console.log(hora)
+    hora++
+    console.log(hora)
 
-        if (hora >= 8 && hora <= 12) {
-            
-            for (let x = 0; x < hora ; x++) {
-    
-             console.log("cuco")
-    
-            }
-        }
-        else {
-             for (let x = 0; x < hora-12; x++) {
-               console.log("cuco");
-             }
-            
-        }
+    if (hora >= 8 && hora <= 12) {
+      for (let j = 0; j < hora; j++) {
+        console.log("cucú")
+      }
     }
+    
+    else {
+      for (let j = 0; j < hora - 12; j++) {
+        console.log("cucú");
+      }
+    }
+  }
 }
